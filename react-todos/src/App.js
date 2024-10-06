8d0a09853e0179419fde32842c2a09c3a3ebae44
Hamza Braim <hamza.braim@outlook.com>
2024-10-06 03:06:18 +0300

    setting up endpoints

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Button } from 'antd';
+// import 'antd/dist/antd.css'; // Correct import for Ant Design CSS
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type='primary'>Button</Button>
     </div>
   );
 }
